a638effdf5f36ab2ac9ef6d29852d2a3ce6c2c68
h-hyakut <hocha01n@gmail.com>
2024-05-23 09:13:14 +0900

    0523 fizzbuzz

--- ruby/ruby2/ruby2_1.rb
+++ ruby/ruby2/ruby2_1.rb
@@ -1,2 +1,12 @@
-puts "neko
-"
+def fizzbuss(num)
+    if num % 3 ==0
+        puts "Fizz"
+    elsif num % 5 ==0
+        puts "Buzz"
+    elsif num % 3 == 0 && num % 5 == 0 #&&:かつ
+        puts "FizzBuzz"
+    else
+        puts num
+    end
+end
+
