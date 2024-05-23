26e529b083c528b85b9ca1ce4267e30601354b62
h-hyakut <hocha01n@gmail.com>
2024-05-23 09:26:46 +0900

    0523 fizzbuzz

--- ruby/ruby2/ruby2_1.rb
+++ ruby/ruby2/ruby2_1.rb
@@ -1,12 +1,14 @@
-def fizzbuss(num)
-    if num % 3 ==0
-        puts "Fizz"
+def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0 #&&:かつ
+        puts "FizzBuzz"
     elsif num % 5 ==0
         puts "Buzz"
-    elsif num % 3 == 0 && num % 5 == 0 #&&:かつ
-        puts "FizzBuzz"
+    elsif num % 3 ==0
+        puts "Fizz"
     else
         puts num
     end
 end
+
+(1..100).each { |num| fizzbuzz(num) }
 
